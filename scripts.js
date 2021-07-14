4c1154a1f45bc62203bdc35dd825454363b9d2f6
Alex van Winkel <alexicoo@gmail.com>
2021-07-14 09:05:04 -0400

    changed celcius to celsius

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
 function convertF2C(){
     var input = $('#fahrenheit').val();
     var output = (input - 32) * 5/9;
-    $("#celcius").html(output.toFixed(1));
+    $("#celsius").html(output.toFixed(1));
 }
 
 function convertC2F(){
-    var input = $('#celcius_input').val();
+    var input = $('#celsius_input').val();
     var output = (input * 1.8) + 32 ;
     $("#fahrenheit_output").html(output.toFixed(1));
 }
